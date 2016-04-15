a7e276b03dd3d430fed495aa7984e87181a4140e
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-15 16:01:19 -0500

    - added Say to the game

--- high_low.php
+++ high_low.php
@@ -3,23 +3,48 @@
 $min = 1;
 $max = 100;
 $rand = rand($min, $max);
-echo $rand .PHP_EOL;
+// echo $rand .PHP_EOL;
 
-echo "Let's play a game.\n";
-fwrite(STDOUT,"Guess my number, it's between 1 and 100\n");
+// echo "Let's play a game.\n";
+// fwrite(STDOUT,"Guess my number, it's between 1 and 100\n");
+// $guess = trim(fgets(STDIN));
+
+// do {
+//     if ($guess > $rand) {
+//         echo "Guess Lower\n";
+//         $guess = trim(fgets(STDIN));
+//     } 
+//     elseif ($guess < $rand) {
+//         echo "Guess Higher\n";
+//         $guess = trim(fgets(STDIN));
+//     }
+//     else {
+//         echo "YOU DID IT!\n";
+//         $guess = trim(fgets(STDIN));
+//     }
+// }while ($guess != $rand);
+`say Let us play a game`;
+sleep(2);
+fwrite(STDOUT,`say can you guess my number it is between 1 and 100`);
 $guess = trim(fgets(STDIN));
 
 do {
     if ($guess > $rand) {
-        echo "Guess Lower\n";
+        `say -v "victoria" Guess Lower`;
         $guess = trim(fgets(STDIN));
     } 
     elseif ($guess < $rand) {
-        echo "Guess Higher\n";
+        `say -v "victoria" Guess Higher`;
         $guess = trim(fgets(STDIN));
     }
     else {
-        echo "YOU DID IT!\n";
-        $guess = trim(fgets(STDIN));
+        `say -v "victoria" you did it`;
     }
 }while ($guess != $rand);
+
+
+
+
+
+
+
