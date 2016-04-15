ba67cc0f00532bf8565bf57612ae8cff78e312d0
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-15 14:59:51 -0500

    added     -do/while loop     -if/else within the do/while loop

--- high_low.php
+++ high_low.php
@@ -2,5 +2,24 @@
 
 $min = 1;
 $max = 100;
+$rand = rand($min, $max);
+echo $rand .PHP_EOL;
 
-echo rand($min, $max) .PHP_EOL;
+echo "Let's play a game.\n";
+fwrite(STDOUT,"Guess my number, it's between 1 and 100\n");
+$guess = trim(fgets(STDIN));
+
+do {
+    if ($guess > $rand) {
+        echo "Guess Lower\n";
+        $guess = trim(fgets(STDIN));
+    } 
+    elseif ($guess < $rand) {
+        echo "Guess Higher\n";
+        $guess = trim(fgets(STDIN));
+    }
+    elseif {
+        echo "YOU DID IT!\n";
+        $guess = trim(fgets(STDIN));
+    }
+}while ($guess != $rand);
