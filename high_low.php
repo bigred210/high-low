53a8eef95a70d6163516c5fdb2a052a1c2bc4f51
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-18 11:15:18 -0500

    added an arguements to the High Low game cli arguement exercise

--- high_low.php
+++ high_low.php
@@ -1,17 +1,21 @@
 <?php
 
-$min = 1;
-$max = 100;
+$min = $argv[1];
+$max = $argv[2];
 $rand = rand($min, $max);
-// echo $rand .PHP_EOL;
+echo $rand .PHP_EOL;
 
 // echo "Let's play a game.\n";
-// fwrite(STDOUT,"Guess my number, it's between 1 and 100\n");
+// fwrite(STDOUT,"Guess my number, it is between {$argv[1]} and {$argv[2]}\n");
 // $guess = trim(fgets(STDIN));
 
 // do {
 //     if ($guess > $rand) {
 //         echo "Guess Lower\n";
+//         $guess = trim(fgets(STDIN));
+//     }
+//     elseif (!is_numeric($guess))b {
+//         echo "Enter a numeric value between {$argv[1]} and {$argv[2]}\n";
 //         $guess = trim(fgets(STDIN));
 //     } 
 //     elseif ($guess < $rand) {
@@ -23,9 +27,19 @@ $rand = rand($min, $max);
 //         $guess = trim(fgets(STDIN));
 //     }
 // }while ($guess != $rand);
+
+if ($argc == 3) {
+    echo "arg1 is {$argv[1]} and arg2 is {$argv[2]}\n";
+} 
+elseif ($argv[1] > $argv[2]) {
+    $min = $argv[2];
+    $max = $argv[1];
+}
+
+
 `say Let us play a game`;
 sleep(2);
-fwrite(STDOUT,`say can you guess my number it is between 1 and 100`);
+fwrite(STDOUT,`say can you guess my number it is between {$argv[1]} and {$argv[2]}`);
 $guess = trim(fgets(STDIN));
 
 do {
@@ -37,6 +51,10 @@ do {
         `say -v "victoria" Guess Higher`;
         $guess = trim(fgets(STDIN));
     }
+    elseif (!is_numeric($guess)) {
+        echo "Enter a numeric value between {$argv[1]} and {$argv[2]}\n";
+        $guess = trim(fgets(STDIN));
+    } 
     else {
         `say -v "victoria" you did it`;
     }
